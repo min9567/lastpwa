8b0e68bfe3f43d7458539e9afa860aa55b0d3a7b
DESKTOP-29VI7GO\user <nju9567@gmail.com>
2025-05-22 12:01:07 +0900

    no message

--- node_work/front01/src/App.jsx
+++ node_work/front01/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import axios from "axios";
+import {useState} from "react";
+import dbusers from "./users.json"
 
 function App() {
+    const [users, setUsers]
+        // = useState([{'id': '홍길동', 'password': '1234'}]);
+        = useState(dbusers);
     const getUsers = async (event) => {
         // console.log(event);
-        const result = await axios.get('http://localhost:8080/users')
-        console.log(result);
+
+        const result = await axios.get('http://localhost:8080')
+        const {data, status} = result;
+
+        setUsers(data);
+
+        // console.log(data);
+        // console.log(status);
+
     }
 
     return (
@@ -14,6 +26,16 @@ function App() {
                 <h1 className={`text-3xl`}>
                     안녕
                 </h1>
+                {
+                    users.map(user =>
+                        (
+                            <div key={user.id}>
+                                <div>{user.id}</div>
+                                <div>{user.password}</div>
+                            </div>
+                        )
+                    )
+                }
                 <button
                     className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out transform hover:-translate-y-0.5 focus:outline-none"
                     onClick={(event) => {
