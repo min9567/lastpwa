3dff40d2d5a3cd3ddd2f177c7425a9bb5efc3993
DESKTOP-29VI7GO\user <nju9567@gmail.com>
2025-07-10 09:52:00 +0900

    no message

--- node_work/backrncs/schemas/index.js
+++ node_work/backrncs/schemas/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require('dotenv').config(); // process env에 설정한걸 갖옴
 
-const mongo_url = "mongodb+srv://nju9567:gc9JcSyDUXr6JWB6@cluster0.4kaug1g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongo_url = process.env.MONGO_URL;
 
 const connect = () => {
     mongoose.connect(mongo_url, { dbName: 'rncs', useNewUrlParser: true, useUnifiedTopology: true })
